b70eef3a99d948aa84140648fd7fdd3d840fb621
Alejandro Uscátegui <uscateguit@gmail.com>
2024-05-08 21:52:21 -0500

    feature :sparkles: Código base funcional

--- rest-input-adapter/src/main/java/co/edu/javeriana/as/personapp/mapper/PersonaMapperRest.java
+++ rest-input-adapter/src/main/java/co/edu/javeriana/as/personapp/mapper/PersonaMapperRest.java
@@ -1,6 +1,7 @@
 package co.edu.javeriana.as.personapp.mapper;
 
 import co.edu.javeriana.as.personapp.common.annotations.Mapper;
+import co.edu.javeriana.as.personapp.domain.Gender;
 import co.edu.javeriana.as.personapp.domain.Person;
 import co.edu.javeriana.as.personapp.model.request.PersonaRequest;
 import co.edu.javeriana.as.personapp.model.response.PersonaResponse;
@@ -27,8 +28,15 @@ public class PersonaMapperRest {
 	}
 
 	public Person fromAdapterToDomain(PersonaRequest request) {
-		// TODO Auto-generated method stub
-		return new Person();
+//		IMPLEMENTAR
+
+		return Person.builder()
+				.identification(Integer.parseInt(request.getDni()))
+				.firstName(request.getFirstName())
+				.lastName(request.getLastName())
+				.age(Integer.parseInt(request.getAge()))
+				.gender(Gender.valueOf(request.getSex()))
+				.build();
 	}
 		
 }
